f73ececc7ef16d53b49d70431146968c1f61a1b8
Savannah-Shifflet <savannah.shifflet@gmail.com>
2023-08-02 13:40:44 -0500

    created get request for notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,18 @@
 const notes = require('express').Router();
+const fs = require('fs');
 
 
 notes.get('/', (req, res) => {
     console.info(`${req.method} request received for notes`);
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if(err){
+            console.log(err);
+            return;
+        }
+        res.json(JSON.parse(data));
+    });
 });
+
+
+module.exports = notes;
